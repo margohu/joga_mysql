9e04af7247e011b2d0add0fd69b96b5c7948bc32
Margo Hutt <margo.hutt@khk.ee>
2022-05-03 21:11:27 +0300

    Added handlebars template engine

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 
 const path = require('path')
+
 // add template engine
 const hbs = require('express-handlebars');
 // setup template engine directory and files extensions
